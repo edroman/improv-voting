6f73e2e172406c731d052c017c88fb7922eef4be
Ed Roman <edward.w.roman@gmail.com>
2012-11-26 22:39:43 -0800

    switched from collections to arrays for Underscore support, to allow for cleaner code

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var async = require('async');				// Allows waterfall cascade of async ops
 var Parse = require('parse').Parse;
 // var Game = require('../models/game').Game;
 var Turn = require('../models/turn').Turn;
+var _ = require('underscore')._;
 
 function parse(req, res)
 {
@@ -13,57 +14,63 @@ function parse(req, res)
 	});
 	var Game = Parse.Object.extend("Game",
 	{
+		className: "Game",
 		turns: Turns
 	});
+	Game.prototype.load = function(callback)
+	{
+		console.log("Game loading turns...");
+
+		// Find all turns related to this game
+		this.turns = new Parse.Query("Turn").equalTo("Game", this).include(["Game.creator", "Game.invitee", "User"]).collection();
+		this.turns.fetch(
+		{
+			success: function(turns)
+			{
+				// For each turn...
+				turns.each( function(turn)
+				{
+					console.log("Game ID: " + turn.get("Game").id + " Turn ID: " + turn.id + " creator = " + turn.get("Game").get("creator").get("name") + " invitee = " + turn.get("Game").get("invitee").get("name") + " User = " + turn.get("User").get("name"));
+				});
+				console.log(callback);
+				callback.success();
+			},
+			error: function(collection, error) { console.log(error); }
+		});
+	};
+
 	var Games = Parse.Collection.extend(
 	{
+		className: "Games",
 		model: Game,
 		query: (new Parse.Query(Game).include(["creator", "invitee"]))
 	});
-
+	
 	// Instantiate the game tree
-	var recentGames = new Games();
+//	var recentGames = new Games();
 	var otherGames = new Games();
 
 	async.waterfall([
 		// 1) Find recent games
 		function(callback) {
-			recentGames.fetch(
+		
+			new Parse.Query(Game).include(["creator", "invitee"]).find(
 			{
 				success: function(games)
 				{
 					// For each game...
-					games.each( function(game)
+					var callNext = _.after(games.length, function() { callback(null, games) } );
+					_.each(games, function(game)
 					{
-						// Find all turns related to this game
-						game.turns = new Parse.Query("Turn").equalTo("Game", game).include(["Game.creator", "Game.invitee", "User"]).collection();
-						game.turns.fetch(
-						{
-							success:
-								function(turns)
-								{
-									// For each turn...
-									turns.each( function(turn)
-									{
-										console.log("Game ID: " + turn.get("Game").id + " Turn ID: " + turn.id + " creator = " + turn.get("Game").get("creator").get("name") + " invitee = " + turn.get("Game").get("invitee").get("name") + " User = " + turn.get("User").get("name"));
-
-										// If we've done an async call to retrieve the last turn of the last game,
-										// then invoke callback so we reply with HTTP response
-										if (turn == turns.last() && game == games.last())
-										{
-											callback(null, recentGames);
-										}
-									});
-								},
-							error: function(collection, error) { console.log(error); }
-						});
+						// Deep load it
+						game.load( { success: callNext } );
 					});
 				},
-				error: function(collection, error) {  console.log(error); }
+				error: function(error) {  console.log(error); }
 			});
 		},
 
-		// 2) Find random games (TODO)
+		// 2) Find random games (TODO - calculate some random numbers and ask for rows whose index -- a new column -- match those numbers)
 		function(recentGames, callback) {
 			otherGames.fetch(
 			{
