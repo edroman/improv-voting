9712d34f5a394ddc211f501c94da8eb22dcd4bc1
Ed Roman <edward.w.roman@gmail.com>
2012-12-05 17:10:28 -0800

    object model refactoring

--- routes/mystories.js
+++ routes/mystories.js
@@ -2,7 +2,10 @@ var async = require('async');				// Allows waterfall cascade of async ops
 var Parse = require('parse').Parse;
 //Parse.initialize("oqMegxam44o7Bnqw0osiRGEkheO9aMHm7mEGrKhb", "TzhNqjKrx2TOpvVqNEh3ppBJmcqMUkBq9AMvBjxi");
 Parse.initialize("WTbIj7pY3jJC3cnqxF2cidV164TOWxgTtbGfjGnF", "l4EnB0wSnIIHUIjjcTiBqsJxHT9zdDVhoTIYSowX");
+var Game = require('../models/game').Game;
+var Games = require('../models/game').Games;
 var Turn = require('../models/turn').Turn;
+var Turns = require('../models/turn').Turns;
 var _ = require('underscore')._;
 
 exports.show = function(req, res)
@@ -12,44 +15,6 @@ exports.show = function(req, res)
 		res.redirect('/');
 		return;
 	}
-
-	var Turns = Parse.Collection.extend(
-	{
-		model: Turn
-	});
-	var Game = Parse.Object.extend("Game",
-	{
-		className: "Game",
-		turns: Turns
-	});
-	Game.prototype.load = function(callback)
-	{
-		console.log("Game loading turns...");
-
-		// Find all turns related to this game
-		this.turns = new Parse.Query("Turn").equalTo("Game", this).include(["Game.creator", "Game.invitee", "User"]).collection();
-		this.turns.fetch(
-		{
-			success: function(turns)
-			{
-				// For each turn...
-				turns.each( function(turn)
-				{
-					console.log("Game ID: " + turn.get("Game").id + " Turn ID: " + turn.id + " creator = " + turn.get("Game").get("creator").get("name") + " invitee = " + turn.get("Game").get("invitee").get("name") + " User = " + turn.get("User").get("name"));
-				});
-				console.log(callback);
-				callback.success();
-			},
-			error: function(collection, error) { console.log(error); }
-		});
-	};
-
-	var Games = Parse.Collection.extend(
-	{
-		className: "Games",
-		model: Game,
-		query: (new Parse.Query(Game).include(["creator", "invitee"]))
-	});
 
 	async.waterfall([
 		// 1) Find games I created
