ac50ebf1e2ca7a20ffdfd857d1e3e5be91de6562
Ed Roman <edward.w.roman@gmail.com>
2012-12-05 16:57:25 -0800

    updated object model

--- models/game.js
+++ models/game.js
@@ -1,21 +1,43 @@
-// var Turn = require('./turn').Turn;
+var Turn = require('./turn').Turn;
+var Turns = require('./turn').Turns;
 var async = require('async');				// Allows waterfall cascade of async ops
 var Parse = require('parse').Parse;
 //Parse.initialize("oqMegxam44o7Bnqw0osiRGEkheO9aMHm7mEGrKhb", "TzhNqjKrx2TOpvVqNEh3ppBJmcqMUkBq9AMvBjxi");
 Parse.initialize("WTbIj7pY3jJC3cnqxF2cidV164TOWxgTtbGfjGnF", "l4EnB0wSnIIHUIjjcTiBqsJxHT9zdDVhoTIYSowX");
 
-var Turns = Parse.Collection.extend(
-{
-	model: Turn
-});
-
 var Game = Parse.Object.extend("Game",
 {
+	className: "Game",
 	turns: Turns
 });
+Game.prototype.load = function(callback)
+{
+	console.log("Game loading turns...");
 
-function Game() {
-	...
+	// Find all turns related to this game
+	this.turns = new Parse.Query("Turn").equalTo("Game", this).include(["Game.creator", "Game.invitee", "User"]).collection();
+	this.turns.fetch(
+	{
+		success: function(turns)
+		{
+			// For each turn...
+			turns.each( function(turn)
+			{
+				console.log("Game ID: " + turn.get("Game").id + " Turn ID: " + turn.id + " creator = " + turn.get("Game").get("creator").get("name") + " invitee = " + turn.get("Game").get("invitee").get("name") + " User = " + turn.get("User").get("name"));
+			});
+			console.log(callback);
+			callback.success();
+		},
+		error: function(collection, error) { console.log(error); }
+	});
 };
+
+var Games = Parse.Collection.extend(
+{
+	className: "Games",
+	model: Game,
+	query: (new Parse.Query(Game).include(["creator", "invitee"]))
+});
  
-module.exports = Game;
+exports.Game = Game;
+exports.Games = Games;
--- routes/index.js
+++ routes/index.js
@@ -5,45 +5,13 @@ Parse.initialize("WTbIj7pY3jJC3cnqxF2cidV164TOWxgTtbGfjGnF", "l4EnB0wSnIIHUIjjcT
 // var Game = require('../models/game').Game;
 var Turn = require('../models/turn').Turn;
 var Turns = require('../models/turn').Turns;
+var Game = require('../models/game').Game;
+var Games = require('../models/game').Games;
 var _ = require('underscore')._;
 
 function parse(req, res)
 {
 	console.log("Current user: " + Parse.User.current() + " req.user: " + req.user);
-
-	var Game = Parse.Object.extend("Game",
-	{
-		className: "Game",
-		turns: Turns
-	});
-	Game.prototype.load = function(callback)
-	{
-		console.log("Game loading turns...");
-
-		// Find all turns related to this game
-		this.turns = new Parse.Query("Turn").equalTo("Game", this).include(["Game.creator", "Game.invitee", "User"]).collection();
-		this.turns.fetch(
-		{
-			success: function(turns)
-			{
-				// For each turn...
-				turns.each( function(turn)
-				{
-					console.log("Game ID: " + turn.get("Game").id + " Turn ID: " + turn.id + " creator = " + turn.get("Game").get("creator").get("name") + " invitee = " + turn.get("Game").get("invitee").get("name") + " User = " + turn.get("User").get("name"));
-				});
-				console.log(callback);
-				callback.success();
-			},
-			error: function(collection, error) { console.log(error); }
-		});
-	};
-
-	var Games = Parse.Collection.extend(
-	{
-		className: "Games",
-		model: Game,
-		query: (new Parse.Query(Game).include(["creator", "invitee"]))
-	});
 	
 	// Instantiate the game tree
 //	var recentGames = new Games();
