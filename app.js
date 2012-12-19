7a866e93f539ec80f631be7e1e2c05b08d47f363
Ed Roman <edward.w.roman@gmail.com>
2012-12-19 04:54:16 -0800

    fixed facebook share code

--- app.js
+++ app.js
@@ -168,7 +168,8 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler());
-});
+  app.locals.pretty = true;}
+);
 
 app.get('/', routes.index);
 app.get('/users', user.list);
